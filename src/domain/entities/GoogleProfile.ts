dc042d0988d4de6ecbe2a076e39caa3494b72728
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-06-21 15:56:07 -0300

    feat(GoogleAuthentication): add contract and entities

---
+++ src/domain/entities/GoogleProfile.ts
@@ -0,0 +1,8 @@
+export type GoogleProfile = {
+  user_id: string;
+  google_account_id: string;
+  email: string;
+  avatar_url?: string;
+  created_at: Date;
+  updated_at: Date;
+};
---
+++ src/domain/entities/User.ts
@@ -0,0 +1,7 @@
+export type User = {
+  id: string;
+  name: string;
+  avatar_url?: string;
+  created_at: Date;
+  updated_at: Date;
+};
