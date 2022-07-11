184162fc3304ea9a9e1960525cb61ff879559718
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-07-11 11:47:52 -0300

    fix(ListAllMonthsByUserControllerValidation): lint

--- src/main/factories/validators/controllers/transaction/ListAllMonthsByUser.ts
+++ src/main/factories/validators/controllers/transaction/ListAllMonthsByUser.ts
@@ -1,8 +1,4 @@
-import { ListAllMonthsByUserController } from '@presentation/controllers/transaction/ListAllMonthsByUser';
-import {
-  RequiredFieldValidation,
-  ValidationComposite,
-} from '@presentation/validations/validators';
+import { ValidationComposite } from '@presentation/validations/validators';
 
 export function makeListAllMonthsByUserControllerValidation(): ValidationComposite {
   return new ValidationComposite([]);
